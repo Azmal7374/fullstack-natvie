753bc7936fc99dad97784753147a24cb9d432c05
Azmal <azmalgazi8@gmail.com>
2025-02-13 21:00:58 +0600

    componets and add some image in asssets file

---
+++ app/_layout.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+ 
+  
+
+ 
+
+  return (
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+  );
+};
+
+export default RootLayout;
