7f0f151d904b32d42e5b83029659fdf87716db8f
Azmal <azmalgazi8@gmail.com>
2025-02-13 19:28:21 +0600

    initialze this project setup done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import "../global.css";
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
