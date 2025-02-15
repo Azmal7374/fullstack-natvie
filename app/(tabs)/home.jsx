883f3843d61ef77fcede1097cd680b71d2ede4b2
Azmal <azmalgazi8@gmail.com>
2025-02-15 16:18:37 +0600

    show data home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,37 +1,87 @@
-import { View, Text, SafeAreaView, FlatList, Image } from 'react-native'
-import React from 'react'
-import { images } from '../../constants'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  RefreshControl,
+
+} from "react-native";
+import {  useState } from "react";
+import { images } from "../../constants";
+import { EmptyState, SearchInput, Trending, VideoCard } from "../../components";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
 
 const Home = () => {
+
+   const {data:posts, refetch} = useAppwrite(getAllPosts)
+   const {data:latestPosts} = useAppwrite(getLatestPosts)
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+  console.log(posts)
   return (
-    <SafeAreaView className="bg-primary">
-     <FlatList  
-     data={[{id:1},{id:2},{id:3}]}
-     keyExtractor={(item)=>item.$id}
-     renderItem={({item})=>(
-         <Text className="text-3xl text-white">{item.id}</Text>
-     )}
-     ListHeaderComponent={()=>(
-      <View className="my-6 px-4 space-y-6">
-      <View className="justify-between items-center flex-row mb-6">
-      <View>
-      <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-      <Text className="font-pmedium text-2xl font-semibold text-white">Native Mastery</Text>
-      </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-center flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="font-pmedium text-2xl font-semibold text-white">
+                  Native Mastery
+                </Text>
+              </View>
 
-      <View className="mt-1.5">
-      <Image className="w-9 h-10" source={images.logoSmall} resizeMode="contain" />
-      </View>
-      </View>
+              <View className="mt-1.5">
+                <Image
+                  className="w-9 h-10"
+                  source={images.logoSmall}
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
 
-
-      
-
-      </View>
-     )}
-     />
+            <SearchInput />
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+              <Trending
+                posts={ latestPosts ?? []}
+              />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
